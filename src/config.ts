bf23af114775bd9d7b4defab8d236c3ce5823c74
Denney Young <denney@flyflower.local>
2026-03-14 00:55:57 +0800

    update someone

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://www.denneyang.com/", // replace this with your deployed domain
+  author: "Denney Yang",
+  profile: "https://www.denneyang.com/",
+  desc: "Record denney's life",
+  title: "Denney's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
